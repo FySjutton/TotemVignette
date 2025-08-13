500de5c6e1140c879fd1df7f74a27648363c0982
Fy17 <rubik.sten@gmail.com>
2025-08-13 22:24:19 +0200

    Port to 1.21.2-1.21.5

--- src/main/java/avox/totemvignette/mixin/InGameHudMixin.java
+++ src/main/java/avox/totemvignette/mixin/InGameHudMixin.java
@@ -5,11 +5,13 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 
+import net.minecraft.util.math.ColorHelper;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -20,12 +22,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(InGameHud.class)
 public class InGameHudMixin {
 	@Final @Shadow private static Identifier VIGNETTE_TEXTURE;
-
 	@Shadow @Final private MinecraftClient client;
 
 	@Inject(
 			method = "renderVignetteOverlay",
-			at = @At(value = "INVOKE", args = "HEAD", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIIFFIIII)V"),
+			at = @At(value = "INVOKE", args = "HEAD", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Ljava/util/function/Function;Lnet/minecraft/util/Identifier;IIFFIIIII)V"),
 			cancellable = true
 	)
 	private void renderFullVignetteIfNoTotem(DrawContext context, Entity entity, CallbackInfo ci) {
@@ -35,24 +36,9 @@ public class InGameHudMixin {
 			boolean hasTotem = player.getMainHandStack().getItem() == Items.TOTEM_OF_UNDYING || player.getOffHandStack().getItem() == Items.TOTEM_OF_UNDYING;
 			if (!hasTotem) {
 				float f = ConfigSystem.CONFIG.instance().vignettePower;
+				int color = ColorHelper.fromFloats(1.0F, 0.0F, f, f);
 
-				context.setShaderColor(0.0F, f, f, 1.0F);
-				context.drawTexture(
-						VIGNETTE_TEXTURE,
-						0,
-						0,
-						-90,
-						0.0F,
-						0.0F,
-						client.getWindow().getScaledWidth(),
-						client.getWindow().getScaledHeight(),
-						client.getWindow().getScaledWidth(),
-						client.getWindow().getScaledHeight()
-				);
-				RenderSystem.depthMask(true);
-				RenderSystem.enableDepthTest();
-				context.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-				RenderSystem.defaultBlendFunc();
+				context.drawTexture(RenderLayer::getVignette, VIGNETTE_TEXTURE, 0, 0, 0.0F, 0.0F, context.getScaledWindowWidth(), context.getScaledWindowHeight(), context.getScaledWindowWidth(), context.getScaledWindowHeight(), color);
 				ci.cancel();
 			}
 		}
